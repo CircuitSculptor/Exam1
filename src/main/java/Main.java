14c4ae17cb2041d42d191435eadc3bfc5f69b39a
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2024-11-06 17:35:07 +0000

    End of exam touchup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,33 @@
 import java.util.Scanner;
+import java.util.Exception;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the customer portal of our company");
-        System.out.println("Please choose one of the following options: ");
-        System.out.println("C - Add Customer\nU - Update Customer\nD - Display All Customers\nE - Exit");
+        System.out.println("Welcome to the customer portal of our company\n" +
+                "Please choose one of the following options: \n" +
+                "C - Add Customer\nU - Update Customer\nD - Display All Customers\nE - Exit");
 
         try (Scanner sc = new Scanner(System.in)) {
             try {
                 int option = sc.nextInt();
-                //char option = sc;
 
                 if (option == 'C') {
-
+                    getDetails();
+                    System.out.println("Add Customer");
 
                 } else if (option == 'U') {
-
+                    System.out.println("Update Customer");
 
                 } else if (option == 'D') {
-
+                    System.out.println("Display All Customers");
 
                 } else if (option == 'E') {
-
+                    System.out.println("Exit");
                 }
             }
             catch (Exception e) {
+                getMessage(e);
                 System.out.println("Invalid option " + getMessage(e));
             }
         }
